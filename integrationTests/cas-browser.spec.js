769e6da8d6f082fd88f8ed96eaa98822ade693b6
Christoph Wolfes <christoph.wolfes@triology.de>
2018-06-19 14:48:10 +0200

    updates to the current webdriver version without selenium stack magic

--- integrationTests/cas-browser.spec.js
+++ integrationTests/cas-browser.spec.js
@@ -1,6 +1,6 @@
 const config = require('./config');
+const utils = require('./utils');
 const expectations = require('./expectations');
-const request = require('supertest');
 const webdriver = require('selenium-webdriver');
 
 const By = webdriver.By;
@@ -13,47 +13,46 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 let driver;
 
 beforeEach(async() => {
-    driver = new webdriver.Builder()
-        .withCapabilities(webdriver.Capabilities.chrome())
-        .build();
+    driver = utils.createDriver(webdriver);
+    await driver.manage().window().maximize();
 });
 
 afterEach(async() => {
-    driver.quit();
+    await driver.quit();
 });
 
 async function login() {
-    driver.get(config.baseUrl + '/scm');
-    driver.findElement(By.id('username')).sendKeys(config.username);
-    driver.findElement(By.id('password')).sendKeys(config.password);
-    driver.findElement(By.css('input[name="submit"]')).click();
+    await driver.get(config.baseUrl + '/scm');
+    await driver.findElement(By.id('username')).sendKeys(config.username);
+    await driver.findElement(By.id('password')).sendKeys(config.password);
+    await driver.findElement(By.css('input[name="submit"]')).click();
 
     // waiting until page is loaded
-    driver.wait(until.elementLocated(By.css('#scm-userinfo-tip')), 5000);
+    await driver.wait(until.elementLocated(By.css('#scm-userinfo-tip')), 5000);
     const userInfoElement = driver.findElement(By.id('scm-userinfo-tip'));
-    driver.wait(until.elementTextIs(userInfoElement, config.username), 5000);
+    await driver.wait(until.elementTextIs(userInfoElement, config.username), 5000);
 
-};
+}
 
 
 describe('cas browser tests', () => {
 
     test('redirect to cas authentication', async() => {
-        driver.get(config.baseUrl + '/scm');
+        await driver.get(config.baseUrl + '/scm');
         const url = await driver.getCurrentUrl();
 
         expectations.expectCasLogin(url);
     });
 
     test('cas authentication', async() => {
-        login();
+        await login();
         const username = await driver.findElement(By.id('scm-userinfo-tip')).getText();
         expect(username).toBe(config.username);
     });
 
     test('check cas attributes', async() => {
-        login();
-        driver.get(config.baseUrl + '/scm/api/rest/authentication/state.json');
+        await login();
+        await driver.get(config.baseUrl + '/scm/api/rest/authentication/state.json');
         const bodyText = await driver.findElement(By.css('body')).getText();
 
         expectations.expectState(JSON.parse(bodyText));
@@ -61,19 +60,19 @@ describe('cas browser tests', () => {
     });
 
     test('front channel logout', async() => {
-        login();
-        driver.wait(until.elementLocated(By.css('#navLogout a'))).click();
-        driver.wait(until.elementLocated(By.css('div#msg.success')));
+        await login();
+        await driver.wait(until.elementLocated(By.css('#navLogout a'))).click();
+        await driver.wait(until.elementLocated(By.css('div#msg.success')));
         const url = await driver.getCurrentUrl();
 
         expectations.expectCasLogout(url);
     });
 
     test('back channel logout', async() => {
-        login();
+        await login();
         await driver.get(config.baseUrl + '/cas/logout');
         await driver.get(config.baseUrl + '/scm');
-        driver.wait(until.elementLocated(By.id('login')));
+        await driver.wait(until.elementLocated(By.id('login')));
         const url = await driver.getCurrentUrl();
         expectations.expectCasLogin(url);
     });
