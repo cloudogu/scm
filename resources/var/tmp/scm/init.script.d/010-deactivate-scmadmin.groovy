085066c9ec04bc3b51bc8b797a333176ab4caa23
René Pfeuffer <rene.pfeuffer@cloudogu.com>
2019-03-13 16:39:39 +0100

    Fix deactivation of scmadmin

--- resources/var/tmp/scm/init.script.d/010-deactivate-scmadmin.groovy
+++ resources/var/tmp/scm/init.script.d/010-deactivate-scmadmin.groovy
@@ -6,6 +6,6 @@ def userManager = injector.getInstance(UserManager.class);
 def scmadmin = userManager.get('scmadmin');
 
 if (scmadmin.type == 'xml'){
-    scmadmin.setActive(true);
+    scmadmin.setActive(false);
     userManager.modify(scmadmin);
 }
