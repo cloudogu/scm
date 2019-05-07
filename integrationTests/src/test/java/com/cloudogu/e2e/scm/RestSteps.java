4eef45646945121e6d15b2a809fa9851c30a784f
René Pfeuffer <rene.pfeuffer@cloudogu.com>
2019-05-07 10:06:26 +0200

    Add test for user attributes via REST

--- integrationTests/src/test/java/com/cloudogu/e2e/scm/RestSteps.java
+++ integrationTests/src/test/java/com/cloudogu/e2e/scm/RestSteps.java
@@ -1,6 +1,9 @@
 package com.cloudogu.e2e.scm;
 
 import com.cloudogu.e2e.Config;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.thoughtworks.gauge.Step;
 
 import javax.net.ssl.HttpsURLConnection;
@@ -8,6 +11,7 @@ import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
@@ -19,14 +23,35 @@ import static com.cloudogu.e2e.Config.BASE_URL;
 
 public class RestSteps {
 
-    @Step("Read repositories with configured username and password")
-    public void readRepositories() throws IOException {
+    @Step("REST: Verify can read repositories with configured username and password")
+    public void verifyReadRepositories() throws IOException {
         URL url = new URL(BASE_URL + "/scm/api/v2/repositories");
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("GET");
         String encoded = Base64.getEncoder().encodeToString((Config.ADMIN_USERNAME + ":" + Config.ADMIN_PASSWORD).getBytes(StandardCharsets.UTF_8));
         connection.setRequestProperty("Authorization", "Basic " + encoded);
         assertThat(connection.getResponseCode()).isEqualTo(200);
+    }
+
+    @Step("REST: Verify values from Me page equal configured values")
+    public void verifyMeData() throws IOException {
+        URL url = new URL(BASE_URL + "/scm/api/v2/me");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        String encoded = Base64.getEncoder().encodeToString((Config.ADMIN_USERNAME + ":" + Config.ADMIN_PASSWORD).getBytes(StandardCharsets.UTF_8));
+        connection.setRequestProperty("Authorization", "Basic " + encoded);
+        assertThat(connection.getResponseCode()).isEqualTo(200);
+        JsonObject content = getContent(connection);
+        assertThat(content.get("name").getAsString()).isEqualTo(Config.ADMIN_USERNAME);
+        assertThat(content.get("displayName").getAsString()).isEqualTo(Config.DISPLAY_NAME);
+        assertThat(content.get("mail").getAsString()).isEqualTo(Config.EMAIL);
+        assertThat(content.get("groups").getAsJsonArray()).extracting(JsonElement::getAsString).contains(Config.ADMIN_GROUP);
+    }
+
+    private JsonObject getContent(HttpURLConnection connection) throws IOException {
+        JsonParser jsonParser = new JsonParser();
+        JsonElement root = jsonParser.parse(new InputStreamReader(connection.getInputStream()));
+        return root.getAsJsonObject();
     }
 
     static {
--- integrationTests/src/test/specs/cas-rest.spec
+++ integrationTests/src/test/specs/cas-rest.spec
@@ -2,4 +2,8 @@
 
 ## Read repositories with basic auth
 
-* Read repositories with configured username and password
+* REST: Verify can read repositories with configured username and password
+
+## Propagate user attributes from CAS
+
+* REST: Verify values from Me page equal configured values
