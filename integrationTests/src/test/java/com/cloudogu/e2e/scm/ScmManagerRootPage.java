ad35eaf38904671f5dca64147c4d19b21c541e07
Eduard Heimbuch <eduard.heimbuch@cloudogu.com>
2019-10-18 15:51:52 +0200

    fix integration tests

--- integrationTests/src/test/java/com/cloudogu/e2e/scm/ScmManagerRootPage.java
+++ integrationTests/src/test/java/com/cloudogu/e2e/scm/ScmManagerRootPage.java
@@ -15,7 +15,7 @@ import static java.util.Optional.of;
 public class ScmManagerRootPage extends Page {
 
     @Required
-    @FindBy(className = "footer")
+    @FindBy(className = "hero-foot")
     WebElement mainMenuEntries;
 
     @FindBy(className = "title")
@@ -32,7 +32,7 @@ public class ScmManagerRootPage extends Page {
     }
 
     static Optional<ScmManagerRootPage> get(WebDriver driver) {
-        if (driver.findElement(By.className("footer")) != null) {
+        if (driver.findElement(By.className("hero-foot")) != null) {
             return of(new ScmManagerRootPage(driver));
         } else {
             return empty();
