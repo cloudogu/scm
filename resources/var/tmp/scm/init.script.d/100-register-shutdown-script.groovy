ae5177db4dd8942a8b474f4c1fef935e0e621a4f
Rene Pfeuffer <rene.pfeuffer@cloudogu.com>
2019-11-22 15:44:55 +0100

    Make shutdown hook synchronous

--- resources/var/tmp/scm/init.script.d/100-register-shutdown-script.groovy
+++ resources/var/tmp/scm/init.script.d/100-register-shutdown-script.groovy
@@ -10,10 +10,10 @@ try {
 	def scriptRepo = injector.getInstance(findClass("sonia.scm.script.domain.StorableScriptRepository"));
 
 	if (scriptRepo.findAll().find { it.title.get().equals("shutdown") } == null) {
-		def script = new StorableScript("Groovy", "Thread.sleep(2000); System.exit(42);")
+		def script = new StorableScript("Groovy", "System.exit(42);")
 		script.setTitle("shutdown");
 		script.setDescription("DO NOT MODIFY THIS!\n\nThis script ensures a restart whenever the internal function fails.")
-		script.setListeners([new Listener(sonia.scm.lifecycle.RestartEvent.class, true)]);
+		script.setListeners([new Listener(sonia.scm.lifecycle.RestartEvent.class, false)]);
 		scriptRepo.store(script);
 	}
 } catch( ClassNotFoundException e ) {
