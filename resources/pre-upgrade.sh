30117a50a30084d1a443cf79cca0b16e56530b94
Rene Pfeuffer <rene.pfeuffer@cloudogu.com>
2019-11-26 08:02:21 +0100

    Fix release version

--- resources/pre-upgrade.sh
+++ resources/pre-upgrade.sh
@@ -5,15 +5,15 @@ set -o nounset
 set -o pipefail
 
 OLD_VERSION="$1"
-NEW_OLD_VERSION="$2"
+NEW_VERSION="$2"
 
 IFS='-' read -ra version_parts <<< "$OLD_VERSION"
 
 echo Found old version "${version_parts[0]}"-"${version_parts[1]}"
 
-# When the old dogu version is less then 2.0.0-30, we have to remove the old plugin folder
+# When the old dogu version is less then 2.0.0-31, we have to remove the old plugin folder
 if [ "${version_parts[0]}" == "2.0.0" ]; then
-  MINOR_VERSION_DELETE_PLUGINS=30
+  MINOR_VERSION_DELETE_PLUGINS=31
   if [ "${version_parts[1]}" -lt "${MINOR_VERSION_DELETE_PLUGINS}" ]; then
     echo "Found old version less 2.0.0-${MINOR_VERSION_DELETE_PLUGINS}. Creating marker file."
     touch /var/lib/scm/plugins/delete_on_update
