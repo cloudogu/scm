dcc06531dc1e69617b554dc76dee92f9675114e2
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2021-10-06 15:20:17 +0200

    Implement release flow

--- build/latestsnapshot.groovy
+++ build/latestsnapshot.groovy
@@ -1,5 +1,8 @@
 import groovy.json.JsonSlurper
 
+GroovyShell shell = new GroovyShell()
+def tools = shell.parse(new File('build/tools.groovy'))
+
 class Package {
 
   String version;
@@ -58,40 +61,6 @@ def appendCesPlugin(env) {
   appendPackage(env, 'SCM_CES_PLUGIN', pkg)
 }
 
-def updateLine(env, line) {
-  int index = line.indexOf('=')
-  if (index > 0) {
-    String left = line.substring(0, index)
-    def key = left.trim()
-    if (key.startsWith("ARG")) {
-      key = key.substring(3).trim()
-    }
-    if (env.containsKey(key)) {
-      String value = env.get(key)
-      String right = line.substring(index + 1)
-      int space = right.indexOf("\s")
-      if (space > 0) {
-        return "${left}=${value}${right.substring(space)}"
-      } else {
-        return "${left}=${value}"
-      }
-    }
-  }
-  return line
-}
-
-def updateDockerfile(env) {
-  def lines = []
-  def file = new File('Dockerfile')
-  file.eachLine { line ->
-    lines.add(updateLine(env, line))
-  }
-  if (!lines.last().empty) {
-    lines.add("")
-  }
-  file.write lines.join("\n")
-}
-
 def normalizeVersion(String version) {
   // 2.23.1-20210928.082411-10
   int index = version.indexOf("-");
@@ -100,22 +69,6 @@ def normalizeVersion(String version) {
     return version.substring(0, index) + "." + snapshotPart.replace("-", "").replace(".", "")
   }
   return version
-}
-
-def updateDoguJson(String version) {
-  def jsonSlurper = new JsonSlurper()
-  def file = new File('dogu.json')
-
-  def content = file.text
-
-  def doguJson = jsonSlurper.parseText(content)
-
-  def oldVersion = doguJson.Version
-
-  def parts = oldVersion.split("-")
-  def newVersion = normalizeVersion(version) + "-" + parts[parts.length -1]
-  
-  file.write content.replace("\"${oldVersion}\"", "\"${newVersion}\"")
 }
 
 def env = [:]
@@ -131,5 +84,9 @@ appendCodeEditorPlugin(env)
 appendScriptPlugin(env)
 appendCesPlugin(env)
 
-updateDockerfile(env)
-updateDoguJson(corePkg.version)
+tools.updateDockerfile(env)
+
+def parts = corePkg.version.split("-")
+def newVersion = normalizeVersion(corePkg.version) + "-" + parts[parts.length -1]
+
+tools.updateDoguJson(newVersion)
---
+++ build/tools.groovy
@@ -0,0 +1,48 @@
+import groovy.json.JsonSlurper
+
+def updateLine(env, line) {
+  int index = line.indexOf('=')
+  if (index > 0) {
+    String left = line.substring(0, index)
+    def key = left.trim()
+    if (key.startsWith("ARG")) {
+      key = key.substring(3).trim()
+    }
+    if (env.containsKey(key)) {
+      String value = env.get(key)
+      String right = line.substring(index + 1)
+      int space = right.indexOf("\s")
+      if (space > 0) {
+        return "${left}=${value}${right.substring(space)}"
+      } else {
+        return "${left}=${value}"
+      }
+    }
+  }
+  return line
+}
+
+def updateDockerfile(env) {
+  def lines = []
+  def file = new File('Dockerfile')
+  file.eachLine { line ->
+    lines.add(updateLine(env, line))
+  }
+  if (!lines.last().empty) {
+    lines.add("")
+  }
+  file.write lines.join("\n")
+}
+
+def updateDoguJson(String newVersion) {
+  def jsonSlurper = new JsonSlurper()
+  def file = new File('dogu.json')
+
+  def content = file.text
+
+  def doguJson = jsonSlurper.parseText(content)
+
+  def oldVersion = doguJson.Version
+  
+  file.write content.replace("\"${oldVersion}\"", "\"${newVersion}\"")
+}
