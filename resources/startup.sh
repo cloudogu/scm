003a2c867fe8c32c0a54d5087cf5711d44f8e4ed
René Pfeuffer <rene.pfeuffer@cloudogu.com>
2024-01-19 09:22:56 +0100

    Re-add restart strategy with exit code 42

--- resources/startup.sh
+++ resources/startup.sh
@@ -45,16 +45,24 @@ fi
 API_TOKEN=$(doguctl random)
 doguctl config --encrypted "${CES_TOKEN_CONFIGURATION_KEY}" "${API_TOKEN}"
 
-# install required plugins
-if ! [ -d "${SCM_DATA}/plugins" ];  then
-  mkdir "${SCM_DATA}/plugins"
-fi
-for plugin in ${SCM_REQUIRED_PLUGINS}; do
-  if { ! [ -d "${SCM_DATA}/plugins/${plugin}" ] || [ -f "${SCM_DATA}/plugins/${plugin}/uninstall" ] ; } && ! [ -f "${SCM_DATA}/plugins/${plugin}.smp" ] ;  then
-    echo "Reinstalling ${plugin} from default plugin folder"
-    cp "${SCM_REQUIRED_PLUGINS_FOLDER}/${plugin}.smp" "${SCM_DATA}/plugins"
+start_scm_server () {
+  # install required plugins
+  if ! [ -d "${SCM_DATA}/plugins" ];  then
+    mkdir "${SCM_DATA}/plugins"
   fi
+  for plugin in ${SCM_REQUIRED_PLUGINS}; do
+    if { ! [ -d "${SCM_DATA}/plugins/${plugin}" ] || [ -f "${SCM_DATA}/plugins/${plugin}/uninstall" ] ; } && ! [ -f "${SCM_DATA}/plugins/${plugin}.smp" ] ;  then
+      echo "Reinstalling ${plugin} from default plugin folder"
+      cp "${SCM_REQUIRED_PLUGINS_FOLDER}/${plugin}.smp" "${SCM_DATA}/plugins"
+    fi
+  done
+  /opt/scm-server/bin/scm-server
+}
+
+# Final startup and restart on exit code 42 (restart event)
+
+while start_scm_server ; scm_exit_code=$? ; [ $scm_exit_code -eq 42 ] ; do
+  echo Got exit code $scm_exit_code -- restarting SCM-Manager
 done
 
-# Start server
-/opt/scm-server/bin/scm-server
+exit $scm_exit_code
